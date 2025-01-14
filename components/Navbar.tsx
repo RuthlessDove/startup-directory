71ff73fb5cf0179f1e980cd00246775306edf23b
Grace <groyleuwumagbe@gmail.com>
2025-01-14 18:06:40 +0000

    Add Navbar component and replace default content in Home page

---
+++ components/Navbar.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">Home</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
