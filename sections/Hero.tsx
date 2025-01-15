f40c062a4e3542da1de32a9927cb6cdcab47f58e
Grace <groyleuwumagbe@gmail.com>
2025-01-15 00:27:42 +0000

    Add Hero component: integrate Navbar into the layout

---
+++ sections/Hero.tsx
@@ -0,0 +1,15 @@
+// Dependencies
+import React from 'react'
+
+// Components
+import Navbar from '@/components/Navbar'
+
+const Hero = () => {
+  return (
+    <div>
+        <Navbar />
+    </div>
+  )
+}
+
+export default Hero
