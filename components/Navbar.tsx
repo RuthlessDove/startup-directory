ffd27cb0ae71e7291640bbe2f2419ab36a701e6d
Grace <groyleuwumagbe@gmail.com>
2025-01-15 00:27:21 +0000

    Refactor Navbar component: add logo, enhance layout, and update navigation items

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,30 +1,37 @@
+// Dependencies
+import Image from "next/image";
 import React from "react";
+
+// Assets
+import logo from "@/assets/icons/logo.svg";
 
 const Navbar = () => {
   return (
     <nav>
-      <div>
-        <ul>
-          <li>
+      <div className="flex justify-center">
+        <ul className="inline-flex flex-row gap-1 mt-2 mx-auto py-2 px-3 border border-white/15 rounded-full font-main ">
+          <li className="pl-3 pr-1 my-auto">
+            <a href="/">
+              <Image src={logo} alt="Logo" />
+            </a>
+          </li>
+          <li className="nav-item">
             <a href="/">Home</a>
           </li>
-          <li>
-            <a href="/">Home</a>
+          <li className="nav-item">
+            <a href="/">About</a>
           </li>
-          <li>
-            <a href="/">Home</a>
+          <li className="nav-item">
+            <a href="/">Directory</a>
           </li>
-          <li>
-            <a href="/">Home</a>
+          <li className="nav-item">
+            <a href="/">FAQ's</a>
           </li>
-          <li>
-            <a href="/">Home</a>
+          <li className="nav-item">
+            <a href="/">Contact</a>
           </li>
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/">Home</a>
+          <li className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900">
+            <a href="/">Join Now</a>
           </li>
         </ul>
       </div>
