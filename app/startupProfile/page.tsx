7109149442b53ab1fd6578dc42ff9c06f6a2ddd7
Grace <groyleuwumagbe@gmail.com>
2025-03-13 21:04:16 +0000

    Add modal for adding updates in startupProfile page

--- app/startupProfile/page.tsx
+++ app/startupProfile/page.tsx
@@ -1,6 +1,17 @@
+"use client";
+
 // Dependencies
 import React from "react";
 import Image from "next/image";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from "@heroui/modal";
+import { useDisclosure } from "@heroui/modal";
+import { Button, ButtonGroup } from "@heroui/button";
 
 // Components
 import Navbar from "@/app/components/Navbar";
@@ -10,8 +21,37 @@ import UpdateEntry from "./components/UpdateEntry";
 import background from "@/assets/images/blue-abstract.jpg";
 import profilePic from "@/assets/images/blank-profile-picture.webp";
 import arrow from "@/assets/icons/boomerang-arrow.svg";
+import { FaPlus } from "react-icons/fa";
 
 const startupProfile = () => {
+  const { isOpen, onOpen, onOpenChange, onClose } = useDisclosure();
+  const [size, setSize] = React.useState<
+    "xs" | "sm" | "md" | "lg" | "xl" | "2xl" | "3xl" | "4xl" | "5xl" | "full"
+  >("4xl");
+
+  interface DisclosureProps {
+    isOpen: boolean;
+    onOpen: () => void;
+    onOpenChange: () => void;
+  }
+
+  const handleOpen = (
+    size:
+      | "xs"
+      | "sm"
+      | "md"
+      | "lg"
+      | "xl"
+      | "2xl"
+      | "3xl"
+      | "4xl"
+      | "5xl"
+      | "full"
+  ) => {
+    setSize(size);
+    onOpen();
+  };
+
   return (
     <div className="mt-7 mx-8">
       <div className="relative">
@@ -42,7 +82,6 @@ const startupProfile = () => {
           </div>
         </div>
       </div>
-
       <div className="bg-gray-200 my-10 px-16 py-8 text-lg rounded-md shadow-md">
         <h2 className="font-display text-2xl font-bold mb-2">
           🛗 Our Elevator Pitch
@@ -53,7 +92,6 @@ const startupProfile = () => {
           real-world applications.
         </p>
       </div>
-
       <div>
         <h2 className="font-display font-bold text-3xl">Pitch Breakdown</h2>
         <p className="text-lg mt-2">
@@ -66,7 +104,6 @@ const startupProfile = () => {
           real-world experience.
         </p>
       </div>
-
       <div className="mt-10">
         <h2 className="font-display font-bold text-3xl mb-4">People</h2>
         <div className="flex gap-8">
@@ -87,15 +124,159 @@ const startupProfile = () => {
           </div>
         </div>
       </div>
-
-      <h2 className="font-display font-bold text-3xl mb-4 mt-10">Updates</h2>
-
+      <div className="flex justify-between items-center mb-4 mt-10">
+        <h2 className="font-display font-bold text-3xl ">Updates</h2>
+        <div>
+          <Button
+            onPress={onOpen}
+            className=" bg-white border rounded-md p-2 flex gap-2 items-center"
+          >
+            <FaPlus />
+            <p className="font-semibold">Add Update</p>
+          </Button>
+        </div>
+      </div>
       <UpdateEntry />
       <UpdateEntry />
-
       <div className="inline-flex gap-2 p-2 items-center bg-gray-200 rounded-lg">
         <Image src={arrow} alt="Arrow pointing down" className="h-5 w-5" />
         <p className="font-semibold">See more</p>
+      </div>
+
+      <div className="">
+        <Modal
+          isOpen={isOpen}
+          size={size}
+          onClose={onClose}
+          className="fixed top-0"
+        >
+          <ModalContent>
+            {(onClose) => (
+              <>
+                <ModalHeader>Add an Update</ModalHeader>
+                <ModalBody>
+                  <form className="max-w-3xl mx-auto">
+                    {/* Personal Details */}
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                      <div>
+                        <label className="block text-sm font-medium text-gray-700">
+                          First Name
+                        </label>
+                        <input
+                          type="text"
+                          className="w-full mt-1 profile-form-input"
+                        />
+                      </div>
+
+                      <div className="col-span-2">
+                        <label className="block text-sm font-medium text-gray-700">
+                          About
+                        </label>
+                        <textarea
+                          rows={2}
+                          className="w-full mt-1 profile-form-input"
+                        ></textarea>
+                      </div>
+                      <div className="col-span-2">
+                        <label className="block text-sm font-medium text-gray-700">
+                          About
+                        </label>
+                        <textarea
+                          rows={2}
+                          className="w-full mt-1 profile-form-input"
+                        ></textarea>
+                      </div>
+                    </div>
+
+                    {/* Socials */}
+                    <div className="border-t-[1px] rounded-full mt-5 mb-3"></div>
+                    <div>
+                      <h3 className="font-semibold text-lg mb-2">Socials</h3>
+                      <div className="flex space-x-2">
+                        {[...Array(3)].map((_, i) => (
+                          <div
+                            key={i}
+                            className="w-8 h-8 bg-gray-200 rounded-md"
+                          ></div>
+                        ))}
+                      </div>
+                    </div>
+
+                    <div className="border-t-[1px] rounded-full mt-5 mb-3"></div>
+
+                    {/* Skills */}
+                    <div>
+                      <h3 className="font-semibold text-lg mb-2">Skills</h3>
+                      <input
+                        type="text"
+                        className="w-1/3 mt-1 profile-form-input mb-2"
+                      />
+
+                      <div className="grid grid-cols-4 gap-2">
+                        <input
+                          type="text"
+                          className="col-span-2 profile-form-input border bg-white"
+                        />
+                        <input
+                          type="text"
+                          className="profile-form-input border bg-white"
+                        />
+                        <input
+                          type="text"
+                          className="profile-form-input border bg-white"
+                        />
+                      </div>
+                    </div>
+
+                    <div className="border-t-[1px] rounded-full mt-5 mb-3"></div>
+                    {/* Startup */}
+                    <div>
+                      <h3 className="font-semibold text-lg mb-2">Startups</h3>
+                      <div className="text-sm font-semibold">Code.ai</div>
+                      <div className="grid grid-cols-2 gap-4 mt-2">
+                        <div>
+                          <label className="block text-sm font-medium text-gray-700">
+                            Role
+                          </label>
+                          <input
+                            type="text"
+                            className="w-full mt-1 profile-form-input"
+                          />
+                        </div>
+                        <div>
+                          <label className="block text-sm font-medium text-gray-700">
+                            Company Email
+                          </label>
+                          <input
+                            type="email"
+                            className="w-full mt-1 profile-form-input"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </form>
+                </ModalBody>
+                <ModalFooter>
+                  <Button
+                    color="danger"
+                    variant="light"
+                    onPress={onClose}
+                    className="font-medium"
+                  >
+                    Discard
+                  </Button>
+                  <Button
+                    color="primary"
+                    onPress={onClose}
+                    className="bg-custom-purple font-semibold"
+                  >
+                    Save
+                  </Button>
+                </ModalFooter>
+              </>
+            )}
+          </ModalContent>
+        </Modal>
       </div>
     </div>
   );
